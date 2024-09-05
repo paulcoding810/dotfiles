e9251d87965e71b62d7ceb9d117a498b68ed2aff
longnghia <41385034+longnghia@users.noreply.github.com>
2024-09-05 14:49:42 +0700

    change hugo editor to zed

--- .zsh/hugo.zsh
+++ .zsh/hugo.zsh
@@ -1,10 +1,10 @@
 blog() {
-	code /Volumes/DATA/hugo/fixthebug
+	zed /Volumes/DATA/hugo/blog
 }
 
 blognew() {
-	cd /Volumes/DATA/hugo/fixthebug
+	cd /Volumes/DATA/hugo/blog
 	# path="content/posts/$1/index.md"
 	hugo new content content/posts/$1/index.md
-	code content/posts/$1/index.md
+	zed content/posts/$1/index.md
 }
