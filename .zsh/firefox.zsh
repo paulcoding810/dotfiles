03988a9c451167c7cd6c716864e665888ba5a6cb
paulcoding810 <longnghia2.00@gmail.com>
2026-05-17 15:47:57 +0700

    firefox: add history

---
+++ .zsh/firefox.zsh
@@ -0,0 +1,33 @@
+add_history() {
+  usage() {
+    echo "Usage: $0 <url> <title>"
+    exit 1
+  }
+
+  if [[ $# -ne 2 ]]; then
+    usage
+  fi
+
+  URL="$1"
+  TITLE="$2"
+
+  PROFILE_DIR="$HOME/Library/Application Support/Firefox/Profiles"
+  PROFILE=$(ls "$PROFILE_DIR" | grep 'default-release' | head -1)
+
+  if [[ -z "$PROFILE" ]]; then
+    echo "Error: no Firefox default-release profile found." >&2
+    exit 1
+  fi
+
+  DB="$PROFILE_DIR/$PROFILE/places.sqlite"
+
+  sqlite3 "$DB" <<SQL
+INSERT INTO moz_places (url, title, visit_count, last_visit_date)
+VALUES ('$URL', '$TITLE', 1, strftime('%s', 'now') * 1000000);
+
+INSERT INTO moz_historyvisits (place_id, visit_date, visit_type)
+VALUES (last_insert_rowid(), strftime('%s', 'now') * 1000000, 1);
+SQL
+
+  echo "Added: $TITLE ($URL)"
+}
